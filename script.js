4d04bff60c75e7783da0ed9acd23a43aaef86c9a
LeilanNaeimi <leilan.n@gmail.com>
2023-12-12 23:48:37 +0300

    add elements on js

---
+++ script.js
@@ -0,0 +1,23 @@
+const btnAC = document.querySelector(".btnAC");
+const btnNeg = document.querySelector(".btnNeg");
+const btnPercent = document.querySelector(".btnPercent");
+const btnDivision = document.querySelector(".btnDivision");
+
+const btn7 = document.querySelector(".btn7");
+const btn8 = document.querySelector(".btn8");
+const btn9 = document.querySelector(".btn9");
+const btnStar = document.querySelector(".btnStar");
+
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btnMinus = document.querySelector(".btnMinus");
+
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btnPlus = document.querySelector(".btnPlus");
+
+const btnZero = document.querySelector(".btnZero");
+const btnDot = document.querySelector(".btnDot");
+const btnEqual = document.querySelector(".btnEqual");
