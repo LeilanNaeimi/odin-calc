e4ce7b84f26b34b6f6858c25ca2eaf4bfa96ad44
LeilanNaeimi <leilan.n@gmail.com>
2023-12-13 12:53:13 +0300

    add numbers on screen

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-const btnAC = document.querySelector(".btnAC");
-const btnNeg = document.querySelector(".btnNeg");
-const btnPercent = document.querySelector(".btnPercent");
-const btnDivision = document.querySelector(".btnDivision");
+// const btnAC = document.querySelector(".btnAC");
+// const btnNeg = document.querySelector(".btnNeg");
+// const btnPercent = document.querySelector(".btnPercent");
+// const btnDivision = document.querySelector(".btnDivision");
 
-const btn7 = document.querySelector(".btn7");
-const btn8 = document.querySelector(".btn8");
-const btn9 = document.querySelector(".btn9");
-const btnStar = document.querySelector(".btnStar");
+// const btn7 = document.querySelector(".btn7");
+// const btn8 = document.querySelector(".btn8");
+// const btn9 = document.querySelector(".btn9");
+// const btnStar = document.querySelector(".btnStar");
 
-const btn4 = document.querySelector(".btn4");
-const btn5 = document.querySelector(".btn5");
-const btn6 = document.querySelector(".btn6");
-const btnMinus = document.querySelector(".btnMinus");
+// const btn4 = document.querySelector(".btn4");
+// const btn5 = document.querySelector(".btn5");
+// const btn6 = document.querySelector(".btn6");
+// const btnMinus = document.querySelector(".btnMinus");
 
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
-const btnPlus = document.querySelector(".btnPlus");
+// const btn1 = document.querySelector(".btn1");
+// const btn2 = document.querySelector(".btn2");
+// const btn3 = document.querySelector(".btn3");
+// const btnPlus = document.querySelector(".btnPlus");
 
-const btnZero = document.querySelector(".btnZero");
-const btnDot = document.querySelector(".btnDot");
-const btnEqual = document.querySelector(".btnEqual");
+// const btn0 = document.querySelector(".btn0");
+// const btnDot = document.querySelector(".btnDot");
+// const btnEqual = document.querySelector(".btnEqual");
+
+const screen = document.querySelector(".screen");
+const buttons = document.querySelectorAll(".btn");
+
+const operators = ["btn+", "btn-", "btn*", "btn/", "btn%"];
+
+let digit = 0;
+let digitStr = "";
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    digit = button.innerHTML;
+
+    console.log(`btn${digit}`);
+
+    switch (e.target.id) {
+      case `btn${digit}`:
+        screen.innerHTML = `${digit}`;
+        console.log(operators);
+        // while (e.target.id !== operators) {
+        //   digitStr += +`${digit}`;
+        //   console.log(digitStr);
+        // }
+
+        break;
+      default:
+        break;
+    }
+  });
+});
